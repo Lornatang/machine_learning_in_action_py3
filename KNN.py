d930a505eec1666995c4f19d2240a4313b639426
shiyipaisizuo <shiyipaisizuo@gmail.com>
2018-05-18 13:26:46 +0800

    fix LICENSE

---
+++ KNN.py
@@ -0,0 +1,8 @@
+from numpy import *
+import operator
+
+
+def createdataset():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
