5d810e8f8744674b27d9a03081a9a0bd1726d473
shiyipaisizuo <shiyipaisizuo@gmail.com>
2018-05-21 11:13:26 +0800

    update logRegres.py

--- src/ch05/logRegres.py
+++ src/ch05/logRegres.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from numpy import *
 
 
+# 加载数据
 def load_data_set():
     data_matrix = []
     label_matrix = []
@@ -26,7 +27,10 @@ def sigmoid(X):
     return 1.0 / (1+exp(-X))
 
 
+# Logistic回归梯度上升优化算法
 def grad_ascent(data_matrix, class_label):
+
+    # 将数据转化为numpy矩阵
     data_matrix = mat(data_matrix)
     label_matrix = mat(class_label).transpose()
     m, n = shape(data_matrix)
@@ -35,6 +39,7 @@ def grad_ascent(data_matrix, class_label):
     max_cycles = 500
     weights = ones((n, 1))
 
+    # 矩阵之间做乘法
     for k in range(max_cycles):
         h = sigmoid(data_matrix*weights)
         error = (label_matrix - h)
@@ -43,6 +48,7 @@ def grad_ascent(data_matrix, class_label):
     return weights
 
 
+# 画图
 def plot_best_fit(weights):
     data_matrix, label_matrix = load_data_set()
     data_array = array(data_matrix)
@@ -66,6 +72,8 @@ def plot_best_fit(weights):
     ax.scatter(x1, y1, s=30, c='red', marker='s')
     ax.scatter(y1, y2, s=30, c='green')
     x = arange(-3.0, 3.0, 0.1)
+
+    # 最佳似合直线
     y = (-weights[0] - weights[1]*x) / weights[2]
     ax.plot(x, y)
     plt.xlabel('X1')
@@ -73,6 +81,7 @@ def plot_best_fit(weights):
     plt.show()
 
 
+# 随机梯度上升算法
 def stroc_grad_ascent0(data_matrix, class_labels):
     m, n = shape(data_matrix)
     alpha = 0.01
@@ -86,6 +95,7 @@ def stroc_grad_ascent0(data_matrix, class_labels):
     return weights
 
 
+# 改进后的随机梯度上升算法
 def stoc_grad_ascent1(data_matrix, class_labels, num_iter=150):
     m, n = shape(data_matrix)
     weights = ones(n)
@@ -93,7 +103,11 @@ def stoc_grad_ascent1(data_matrix, class_labels, num_iter=150):
     for j in range(num_iter):
         data_index = range(m)
         for i in range(m):
+
+            # 每次来调整alpha的值
             alpha = 4 / (1.0 + j + i) + 0.0001
+
+            # 随机选取更新
             rand_index = int(random.uniform(0, len(data_index)))
             h = sigmoid(sum(data_matrix[rand_index] * weights))
             error = class_labels[rand_index] - h
@@ -103,6 +117,7 @@ def stoc_grad_ascent1(data_matrix, class_labels, num_iter=150):
     return weights
 
 
+# Logistic回归分类函数
 def classify_vector(inx, weights):
     prob = sigmoid(sum(inx * weights))
 
@@ -115,8 +130,9 @@ def classify_vector(inx, weights):
 def colic_test():
     frtrain = open('horseColicTraining.txt');
     frtest = open('horseColicTest.txt')
-    train_set = [];
+    train_set = []
     train_labels = []
+
     for line in frtrain.readlines():
         curr_line = line.strip().split('\t')
         line_array = []
@@ -124,9 +140,11 @@ def colic_test():
             line_array.append(float(curr_line[i]))
         train_set.append(line_array)
         train_labels.append(float(curr_line[21]))
+
     train_weights = stoc_grad_ascent1(array(train_set), train_labels, 1000)
     error_count = 0
     num_test_vector = 0.0
+
     for line in frtest.readlines():
         num_test_vector += 1.0
         curr_line = line.strip().split('\t')
@@ -135,14 +153,19 @@ def colic_test():
             line_array.append(float(curr_line[i]))
         if int(classify_vector(array(line_array), train_weights)) != int(curr_line[21]):
             error_count += 1
+
     error_rate = (float(error_count) / num_test_vector)
     print("the error rate of this test is: {}").format(error_rate)
+
     return error_rate
 
 
+# 多重测试
 def mul_test():
-    num_tests = 10;
+    num_tests = 10
     error_sum = 0.0
+
     for k in range(num_tests):
         error_sum += colic_test()
+
     print("after {} iterations the average error rate is: {}").format(num_tests, error_sum / float(num_tests))
